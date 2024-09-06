0f70c0d715da5fc7fed1d0ae89ab1415af8442e9
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-09-06 19:56:31 +0530

    genre controller

--- BookStoreFullStackNg.Api/Controllers/GenreController.cs
+++ BookStoreFullStackNg.Api/Controllers/GenreController.cs
@@ -1,10 +1,13 @@
 using AutoMapper;
+using BookStoreFullStackNg.Api.Exceptions;
+using BookStoreFullStackNg.Data.Domain;
+using BookStoreFullStackNg.Data.DTOs;
 using BookStoreFullStackNg.Data.Reopositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookStoreFullStackNg.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/genres")]
     [ApiController]
     public class GenreController : ControllerBase
     {
@@ -15,6 +18,61 @@ namespace BookStoreFullStackNg.Api.Controllers
         {
             _mapper = mapper;
             _genreRepository = genreRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateGenre(GenreCreateDto genreToAdd)
+        {
+            var genre = _mapper.Map<Genre>(genreToAdd);
+            var createdGenre = await _genreRepository.AddGenre(genre);
+            return CreatedAtAction(nameof(GetGenreById), new { id = createdGenre.Id }, _mapper.Map<GenreReadDto>(createdGenre));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetGenres()
+        {
+            var genres = await _genreRepository.GetGenres();
+            return Ok(_mapper.Map<IEnumerable<GenreReadDto>>(genres));
+        }
+
+        [HttpGet("{id}", Name = "GetGenreById")]
+        public async Task<IActionResult> GetGenreById(int id)
+        {
+            var genre = await _genreRepository.GetGenreById(id);
+            if (genre == null)
+            {
+                throw new NotFoundException("Genre not found");
+            }
+            return Ok(_mapper.Map<GenreReadDto>(genre));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGenre(int id)
+        {
+            var genre = await _genreRepository.GetGenreById(id);
+            if (genre == null)
+            {
+                throw new NotFoundException("Genre not found");
+            }
+            await _genreRepository.DeleteGenre(genre);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateGenre(int id, GenreUpdateDto genreToUpdate)
+        {
+            if (id != genreToUpdate.Id)
+            {
+                throw new BadRequestException("Id mismatch");
+            }
+            var genre = await _genreRepository.GetGenreById(genreToUpdate.Id);
+            if (genre == null)
+            {
+                throw new NotFoundException("Genre not found");
+            }
+            genre = _mapper.Map(genreToUpdate, genre);
+            var updatedGenre = await _genreRepository.UpdateGenre(genre);
+            return Ok(_mapper.Map<GenreReadDto>(updatedGenre));
         }
     }
 }
