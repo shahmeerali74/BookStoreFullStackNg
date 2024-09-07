2248caa7258f9aa7ca17b5a0d5ef5a96c5c3be5d
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-09-07 15:50:10 +0530

    few more integration tests

--- BookStoreFullStackNg.IntegratedTests/GenreControllerTests.cs
+++ BookStoreFullStackNg.IntegratedTests/GenreControllerTests.cs
@@ -1,4 +1,7 @@
+using System.Net;
+using System.Text;
 using System.Text.Json;
+using BookStoreFullStackNg.Data.Domain;
 using BookStoreFullStackNg.Data.DTOs;
 
 namespace BookStoreFullStackNg.IntegratedTests;
@@ -8,11 +11,13 @@ public class GenreControllerTests : IClassFixture<CustomWebApplicationFactory<Pr
 
     private readonly CustomWebApplicationFactory<Program> _factory;
     private readonly HttpClient _client;
+    private string baseUrl;
 
     public GenreControllerTests(CustomWebApplicationFactory<Program> factory)
     {
         _factory = factory;
         _client = _factory.CreateClient();
+        baseUrl = "api/genres";
     }
 
     [Fact]
@@ -21,7 +26,7 @@ public class GenreControllerTests : IClassFixture<CustomWebApplicationFactory<Pr
         // Arrange
 
         // Act
-        var response = await _client.GetAsync("api/genres");
+        var response = await _client.GetAsync(baseUrl);
 
         response.EnsureSuccessStatusCode();
         var responseString = await response.Content.ReadAsStringAsync();
@@ -32,5 +37,102 @@ public class GenreControllerTests : IClassFixture<CustomWebApplicationFactory<Pr
         // Assert
         Assert.NotNull(genres);
         Assert.NotEmpty(genres);
+    }
+
+    [Fact]
+    public async Task GetGenreById_ReturnsOk_WhenGenreExists()
+    {
+        // Arrange
+        var genreId = 1;
+
+        // Act
+        var response = await _client.GetAsync(baseUrl + "/" + genreId);
+
+        response.EnsureSuccessStatusCode();
+
+        var responseString = await response.Content.ReadAsStringAsync();
+        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        var genre = JsonSerializer.Deserialize<GenreReadDto>(responseString, options);
+
+        // Assert
+        Assert.NotNull(genre);
+        Assert.Equal(genre.Id, genreId);
+    }
+
+    [Fact]
+    public async Task GenGenreById_ReturnNotFound_WhenPersonDoesNotExist()
+    {
+        // Arrange
+        int id = 999;
+
+        // Act
+        var response = await _client.GetAsync(baseUrl + "/" + id);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task AddGenre_ReturnsCreatedAtRoute()
+    {
+        // arrange
+        var newGenre = new GenreCreateDto { GenreName = "G1" };
+        var content = new StringContent(JsonSerializer.Serialize(newGenre), Encoding.UTF8, "application/json");
+
+        // Act
+        var response = await _client.PostAsync(baseUrl, content);
+        response.EnsureSuccessStatusCode();
+
+        var jsonResponse = await response.Content.ReadAsStringAsync();
+
+        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        var createdGenre = JsonSerializer.Deserialize<Genre>(jsonResponse, options);
+
+        // Assert
+        Assert.NotNull(createdGenre);
+        Assert.Equal(newGenre.GenreName, createdGenre.GenreName);
+    }
+
+    [Fact]
+    public async Task UpdateGenre_ReturnsNotContent_WhenSuccessfull()
+    {
+        // arrange
+        var genreToUpdate = new GenreUpdateDto { Id = 1, GenreName = "G1" };
+        var content = new StringContent(JsonSerializer.Serialize(genreToUpdate), Encoding.UTF8, "application/json");
+
+        // act
+        var response = await _client.PutAsync($"{baseUrl}/1", content);
+        response.EnsureSuccessStatusCode();
+
+        // assert
+        Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task UpdateGenre_ReturnsBadRequest_WhenIdMismatch()
+    {
+        // arrange
+        var genre = new GenreUpdateDto { Id = 1, GenreName = "G1" };
+        var content = new StringContent(JsonSerializer.Serialize(genre), Encoding.UTF8, "application/json");
+
+        // act
+        var response = await _client.PutAsync($"{baseUrl}/2", content);
+
+        // assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task UpdateGenre_ReturnsNotFound_WhenGenreDoesNotExists()
+    {
+        // arrange
+        var genre = new GenreUpdateDto { Id = 999, GenreName = "G1" };
+        var content = new StringContent(JsonSerializer.Serialize(genre), Encoding.UTF8, "application/json");
+
+        // act
+        var response = await _client.PutAsync($"{baseUrl}/999", content);
+
+        // assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
     }
 }
