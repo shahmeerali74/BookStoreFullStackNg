5d29743ed067e4962b67a3fed7b29b7068ac4fcf
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-09-08 13:08:34 +0530

    ngrx store is added

--- bookstore-fullstack-ng-client/src/app/app.config.ts
+++ bookstore-fullstack-ng-client/src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideStore } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideStore(reducers, { metaReducers })]
 };
