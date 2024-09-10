ced39dce73bbe402b0a02223457000b35e41b6d9
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-09-10 17:23:49 +0530

    registeration service added

---
+++ bookstore-fullstack-ng-client/src/app/account/data/account.service.ts
@@ -0,0 +1,21 @@
+import { inject, Injectable } from "@angular/core";
+import { environment } from "../../../environments/environment.development";
+import { RegistrationModel } from "./registration.model";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({ providedIn: "root" })
+export class AccountService {
+  private readonly baseUrl = environment.baseUrl + "/api/account";
+  private readonly http = inject(HttpClient);
+
+  /**
+   * Registers a new user.
+   *
+   * @param data The user registration data.
+   *
+   * @returns The registered user.
+   */
+  registration(data: RegistrationModel) {
+    return this.http.post<RegistrationModel>(this.baseUrl + "/register", data);
+  }
+}
