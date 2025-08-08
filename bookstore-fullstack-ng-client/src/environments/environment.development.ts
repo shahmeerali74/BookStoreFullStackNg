5d5ecde15d9da5bf75cbfa4c07293029a4b30715
shahmeer <111519383+shahmeerali74@users.noreply.github.com>
2025-08-08 18:42:58 +0500

    Update environment.development.ts

--- bookstore-fullstack-ng-client/src/environments/environment.development.ts
+++ bookstore-fullstack-ng-client/src/environments/environment.development.ts
@@ -1,3 +1,7 @@
+const hostname = window.location.hostname;
+
 export const environment = {
-  baseUrl: "https://localhost:5001",
+  baseUrl: hostname === 'localhost'
+  ? 'http://localhost:5001':
+    'http://135.222.42.11:5001'
 };
